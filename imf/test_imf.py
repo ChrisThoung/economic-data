f3a362370b8b846d4492fe038e71d591f7811ba7
Chris Thoung <chris.thoung@gmail.com>
2024-10-18 23:41:07 +0100

    DOC: Add comments to `infer_encoding()` test

--- imf/test_imf.py
+++ imf/test_imf.py
@@ -14,19 +14,19 @@ class TestTSV(unittest.TestCase):
     def test_infer_encoding(self):
         # Check that `infer_encoding()` identifies the correct encoding
         test_cases = {
-            'WEOSep2011all.xls': 'ISO-8859-1',
+            'WEOSep2011all.xls': 'ISO-8859-1',  # Special case: No other IMF release in September
             'WEOApr2019all.xls': 'ISO-8859-1',
             'WEOOct2019all.xls': 'ISO-8859-1',
             'WEOApr2020all.xls': 'ISO-8859-1',
-            'WEOOct2020all.xls': 'utf-16le',
+            'WEOOct2020all.xls': 'utf-16le',    # Special case: One-off difference, October 2020
             'WEOApr2021all.xls': 'ISO-8859-1',
             'WEOOct2021all.xls': 'ISO-8859-1',
-            'WEOApr2022all.xls': 'utf-16le',
+            'WEOApr2022all.xls': 'utf-16le',    # April forecasts switch file encoding from now on
             'WEOOct2022all.xls': 'ISO-8859-1',
             'WEOApr2023all.xls': 'utf-16le',
             'WEOOct2023all.xls': 'ISO-8859-1',
             'WEOApr2024all.xls': 'utf-16le',
-        }
+        }  # fmt: skip
 
         for name, expected_encoding in test_cases.items():
             with self.subTest(name=name):
